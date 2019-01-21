f03e161bab53122d35f985cceba6be5882d6de7c
Ice wraith Hahn <ihahn@e3r7p14.21-school.ru>
2019-01-21 20:13:56 +0300

    draft

--- libft.h
+++ libft.h
@@ -15,17 +15,18 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
-size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
-char	*stpcpy(char * dst, const char * src);
-char	*strcat(char *restrict s1, const char *restrict s2);
-char	*strncat(char *restrict s1, const char *restrict s2, size_t n);
+char	*ft_stpcpy(char * dst, const char * src);
+char	*ft_strncpy(char *dst, char *src, size_t len)
+char	*ft_strcat(char *restrict s1, const char *restrict s2);
 char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	*ft_strchr(const char *s, int c)
-char	*strstr(const char *haystack, const char *needle);
-char	*strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
