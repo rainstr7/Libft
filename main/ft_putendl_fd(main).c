c93e14f4e0833a96a511bda89e3fd421fd190fd2
rainstr7 <rainstr7@gmail.com>
2019-02-09 19:02:51 +0300

    Final

--- main/ft_putendl_fd(main).c
+++ main/ft_putendl_fd(main).c
@@ -1,27 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_putendl_fd(main).c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ihahn <ihahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/17 17:35:02 by ihahn             #+#    #+#             */
-/*   Updated: 2019/02/09 18:26:41 by ihahn            ###   ########.fr       */
+/*   Created: 2019/01/20 21:00:40 by ihahn             #+#    #+#             */
+/*   Updated: 2019/02/09 17:03:55 by ihahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <unistd.h>
 
-void				ft_bzero(void *s, size_t n)
+int	ft_strlen(const char *s)
 {
-	unsigned char	*str;
-	int				i;
+	int	i;
 
 	i = 0;
-	str = (unsigned char *)s;
-	while (n--)
-	{
-		str[i] = 0;
+	while (s[i] != '\0')
 		i++;
-	}
+	return (i);
+}
+
+void	ft_putendl_fd(char const *s, int fd)
+{
+	int l;
+
+	l = ft_strlen(s);
+	write(fd, s, l);
+	write(fd, "\n", 1);
+}
+
+int main(void)
+{
+	const char app[1024] = "1234567890";
+	ft_putendl_fd(app, 1);
+	return (0);
 }
