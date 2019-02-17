15e4065567091cf4db1580ca95907c88d5bfe374
Ice wraith Hahn <ihahn@e3r7p14.21-school.ru>
2019-02-17 14:28:16 +0300

    17.02.19

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ihahn <ihahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/20 20:44:31 by ihahn             #+#    #+#             */
-/*   Updated: 2019/02/17 12:49:18 by ihahn            ###   ########.fr       */
+/*   Updated: 2019/02/17 14:27:41 by ihahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int			ft_nbrlen(int n)
 {
+	int		num;
 	int		i;
-	int		nb;
-
+	
 	i = 0;
-	nb = n;
+	num = n;
 	if (n == 0)
 		return (1);
-	while (nb)
+	while (num)
 	{
-		nb /= 10;
+		num /= 10;
 		i++;
 	}
 	return ((n < 0) ? i + 1 : i);
@@ -31,11 +31,11 @@ int			ft_nbrlen(int n)
 
 char		*ft_itoa(int n)
 {
-	int		val;
+	int		num;
 	int		len;
 	char	*tmp;
 
-	val = n;
+	num = n;
 	len = ft_nbrlen(n);
 	tmp = ft_strnew(len);
 	while (len--)
@@ -43,7 +43,7 @@ char		*ft_itoa(int n)
 		tmp[len] = (n < 0) ? (n % 10) * -1 + 48 : (n % 10) + 48;
 		n /= 10;
 	}
-	if (val < 0)
+	if (num < 0)
 		tmp[0] = '-';
 	return (tmp);
 }
