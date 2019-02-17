fd6ad208958ac65f17aef0e5f906f29ddf450d04
Ice wraith Hahn <ihahn@e3r7p14.21-school.ru>
2019-02-17 15:21:32 +0300

    17.02.19

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,22 +6,22 @@
 /*   By: ihahn <ihahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/19 17:33:44 by ihahn             #+#    #+#             */
-/*   Updated: 2019/02/17 14:14:18 by ihahn            ###   ########.fr       */
+/*   Updated: 2019/02/17 15:20:45 by ihahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char		*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
 	int		i;
-	int		len2;
+	size_t	len2;
 	char	*str1;
 	char	*str2;
 
 	i = 0;
-	str1 = (char *)haystack;
-	str2 = (char *)needle;
+	str1 = (char *)s1;
+	str2 = (char *)s2;
 	len2 = ft_strlen(str2);
 	if (!str2)
 		return (str1);
